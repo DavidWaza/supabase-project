f93f579e2e61271e6d36f6a69acf8414b090f7cf
David Waza <idemetoemediong2022@gmail.com>
2025-11-13 16:24:35 +0100

    fix: RLS with supabase

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,7 @@ import SmoothieCard from "../components/SmoothieCard";
 const Home = () => {
   const [fetchError, setFetchError] = useState(null);
   const [smoothies, setSmoothies] = useState(null);
+  const [orderBy, setOrderBy] = useState("created_at");
 
   const handleDelete = (id) => {
     setSmoothies((prevSmoothies) => {
@@ -15,7 +16,10 @@ const Home = () => {
 
   useEffect(() => {
     const fetchSmoothies = async () => {
-      const { data, error } = await supabase.from("portfolio").select();
+      const { data, error } = await supabase
+        .from("portfolio")
+        .select()
+        .order(orderBy, { ascending: false });
 
       if (error) {
         setFetchError("Could not fetch the smoothies");
@@ -28,13 +32,21 @@ const Home = () => {
       }
     };
     fetchSmoothies();
-  }, []);
+  }, [orderBy]);
 
   return (
     <div className="page home">
       {fetchError && <p>{fetchError}</p>}
       {smoothies && (
         <div className="smoothies">
+          <div className="order-by">
+            <p>Order By:</p>
+            <button onClick={() => setOrderBy("created_at")}>
+              Time Created
+            </button>
+            <button onClick={() => setOrderBy("title")}>Title</button>
+            <button onClick={() => setOrderBy("rating")}>Rating</button>
+          </div>
           <div className="smoothie-grid">
             {smoothies.map((smoothie) => (
               <SmoothieCard
