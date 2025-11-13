6af47311cec865b144d64e2b27810d4cceace043
David Waza <idemetoemediong2022@gmail.com>
2025-11-13 15:58:26 +0100

    initial commit starter project

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,53 @@
+"use client";
+import supabase from "../config/supabaseClient";
+import { useState, useEffect } from "react";
+import SmoothieCard from "../components/SmoothieCard";
+
 const Home = () => {
+  const [fetchError, setFetchError] = useState(null);
+  const [smoothies, setSmoothies] = useState(null);
+
+  const handleDelete = (id) => {
+    setSmoothies((prevSmoothies) => {
+      return prevSmoothies.filter((sm) => sm.id !== id);
+    });
+  };
+
+  useEffect(() => {
+    const fetchSmoothies = async () => {
+      const { data, error } = await supabase.from("portfolio").select();
+
+      if (error) {
+        setFetchError("Could not fetch the smoothies");
+        setSmoothies(null);
+        console.log(error);
+      }
+      if (data) {
+        setSmoothies(data);
+        setFetchError(null);
+      }
+    };
+    fetchSmoothies();
+  }, []);
+
   return (
     <div className="page home">
-      <h2>Home</h2>
+      {fetchError && <p>{fetchError}</p>}
+      {smoothies && (
+        <div className="smoothies">
+          <div className="smoothie-grid">
+            {smoothies.map((smoothie) => (
+              <SmoothieCard
+                key={smoothie.id}
+                smoothie={smoothie}
+                onDelete={handleDelete}
+              />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/pages/Update.js
+++ src/pages/Update.js
@@ -1,9 +1,90 @@
-const Update = () => {
-  return (
-    <div className="page update">
-      <h2>Update</h2>
-    </div>
-  )
-}
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import supabase from "../config/supabaseClient";
 
-export default Update
+const Update = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const [title, setTitle] = useState("");
+  const [method, setMethod] = useState("");
+  const [rating, setRating] = useState("");
+  const [formError, setFormError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!title || !method || !rating) {
+      setFormError("Please fill in all the fields correctly");
+      return;
+    }
+
+    const { data, error } = await supabase
+      .from("portfolio")
+      .update({ title, method, rating })
+      .eq("id", id)
+      .select();
+
+    if (error) {
+      setFormError("Please fill in the form");
+    }
+    if (data) {
+      setFormError(null);
+      navigate('/')
+    }
+  };
+
+  useEffect(() => {
+    const fetchSmoothies = async () => {
+      const { data, error } = await supabase
+        .from("portfolio")
+        .select()
+        .eq("id", id)
+        .single();
+
+      if (error) {
+        navigate("/", { replace: true });
+      }
+      if (data) {
+        setTitle(data.title);
+        setMethod(data.method);
+        setRating(data.rating);
+        console.log(data);
+      }
+    };
+    fetchSmoothies();
+  }, [id, navigate]);
+
+  return (
+    <div className="page create">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Title:</label>
+        <input
+          type="text"
+          id="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <label htmlFor="method">Method:</label>
+        <textarea
+          id="method"
+          value={method}
+          onChange={(e) => setMethod(e.target.value)}
+        />
+
+        <label htmlFor="rating">Rating:</label>
+        <input
+          type="number"
+          id="rating"
+          value={rating}
+          onChange={(e) => setRating(e.target.value)}
+        />
+
+        <button>Update Smoothie Recipe</button>
+      </form>
+    </div>
+  );
+};
+
+export default Update;
